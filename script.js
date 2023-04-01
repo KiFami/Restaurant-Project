c9f9eabdd7a049d74aa6104fd41186473a54cd00
unknown <KacperPR@interia.pl>
2023-04-01 20:41:33 +0200

    Add js

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function Hamburger(x)
 {
     x.classList.toggle("change");
 }
+
+function showPage(pageName) 
+{
+    // Ukryj wszystkie strony
+    var pages = document.querySelectorAll('.page');
+    for (var i = 0; i < pages.length; i++) 
+    {
+      pages[i].style.display = 'none';
+    }
+  
+    // Pokaż tylko wybraną stronę
+    var page = document.querySelector('#' + pageName);
+    if (page) 
+    {
+      page.style.display = 'block';
+    }
+}
